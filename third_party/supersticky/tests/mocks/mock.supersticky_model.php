508ce7e08149df41d56a7c4170a350c6539ab029
Stephen Lewis <stephen@experienceinternet.co.uk>
2011-10-01 11:27:59 +0100

    Add recently-created model method to mock model.

--- third_party/supersticky/tests/mocks/mock.supersticky_model.php
+++ third_party/supersticky/tests/mocks/mock.supersticky_model.php
@@ -14,11 +14,18 @@ class Mock_supersticky_model {
     public function get_package_theme_url() {}
     public function get_package_version() {}
     public function get_site_id() {}
+    public function get_supersticky_entry_by_id($entry_id) {}
     public function install_module() {}
     public function install_module_actions() {}
     public function install_module_register() {}
+    public function install_module_tables() {}
+    public function install_module_tabs() {}
+    public function save_supersticky_entry(Supersticky_entry $entry) {}
     public function uninstall_module() {}
     public function update_module($installed_version = '') {}
+
+    public function update_supersticky_entry_with_post_data(
+      Supersticky_entry $entry) {}
 
 }
 
