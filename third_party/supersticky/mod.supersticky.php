0e4cd62777c9ff0493665d17c2b91570c5f0aa3d
Stephen Lewis <stephen@experienceinternet.co.uk>
2011-10-18 09:49:24 -0400

    Override Channel class SQL to use SuperSticky for ordering.

--- third_party/supersticky/mod.supersticky.php
+++ third_party/supersticky/mod.supersticky.php
@@ -12,62 +12,95 @@ require_once APPPATH .'modules/channel/mod.channel.php';
 
 class Supersticky extends Channel {
     
-    private $_model;
-    public $return_data = '';
-    
-    
-    /* --------------------------------------------------------------
-     * PUBLIC METHODS
-     * ------------------------------------------------------------ */
+  private $_model;
+  public $return_data = '';
+  
+  
+  /* --------------------------------------------------------------
+   * PUBLIC METHODS
+   * ------------------------------------------------------------ */
 
-    /**
-     * Constructor.
-     *
-     * @access  public
-     * @return  void
-     */
-    public function __construct()
+  /**
+   * Constructor.
+   *
+   * @access  public
+   * @return  void
+   */
+  public function __construct()
+  {
+    parent::__construct();
+
+    $this->EE->load->model('supersticky_model');
+    $this->_model = $this->EE->supersticky_model;
+  }
+  
+  
+  /* --------------------------------------------------------------
+   * TEMPLATE TAG METHODS
+   * ------------------------------------------------------------ */
+  
+  /**
+   * Overrides the 'build' SQL query method.
+   *
+   * @access  public
+   * @param   string      $qstring        Query string.
+   * @return  void
+   */
+  public function build_sql_query($qstring = '')
+  {
+    // parent::build_sql_query($qstring);
+
+    $group_id = $this->EE->session->userdata('group_id');
+    $sql      = $this->sql;
+
+    // Retrieve all the SuperSticky criteria for the current date.
+    $ss_entries = $this->_model->get_supersticky_entries_for_date(
+      new DateTime());
+
+    $ss_length  = count($ss_entries);
+    $ss_items   = array();
+    $where_sql  = '';
+
+    for ($count = 1; $count <= $ss_length; $count++)
     {
-        parent::__construct();
+      $ss_entry = $ss_entries[$count - 1];
+      $include  = FALSE;
 
-        $this->EE->load->model('supersticky_model');
-        $this->_model = $this->EE->supersticky_model;
+      // Does this criterion apply to the current member group.
+      foreach ($ss_entry->get_criteria() AS $criterion)
+      {
+        if (in_array($group_id, $criterion->get_member_groups()))
+        {
+          $ss_items[] = ($count === 1)
+            ? "SELECT {$ss_entry->get_entry_id()} AS entry_id,
+              {$count} AS order_index"
+            : "SELECT {$ss_entry->get_entry_id()}, {$count}";
+
+          break;
+        }
+      }
     }
-    
-    
-    /* --------------------------------------------------------------
-     * TEMPLATE TAG METHODS
-     * ------------------------------------------------------------ */
-    
-    /**
-     * Overrides the 'build' SQL query method.
-     *
-     * @access  public
-     * @param   string      $qstring        Query string.
-     * @return  void
-     */
-    public function build_sql_query($qstring = '')
+
+    if ($ss_items)
     {
-        parent::build_sql_query($qstring);
+      if (count($ss_items) > 1)
+      {
+        $ss_items[0] .= ' UNION ALL';
+      }
 
-        // Customise the SQL.
-        $sql = $this->sql;
-
-        $select = 'SELECT IFNULL(ss.order_index, 999999) AS ss_order_index,';
-
-        $where = 'LEFT JOIN
-            (SELECT 1 AS entry_id, 10 AS order_index UNION ALL SELECT 2, 20)
-            AS ss ON ss.entry_id = t.entry_id
-            WHERE';
-
-        $order = 'ORDER BY ss_order_index ASC,';
-
-        $sql = str_replace('SELECT', $select, $sql);
-        $sql = str_replace('WHERE', $where, $sql);
-        $sql = str_replace('ORDER BY', $order, $sql);
-
-        $this->sql = $sql;
+      $where_sql = 'LEFT JOIN (' .implode(' ', $ss_items) .')
+        AS ss ON ss.entry_id = t.entry_id WHERE';
     }
+
+    $select_sql = 'SELECT IFNULL(ss.order_index, 999999) AS ss_order_index,';
+    $order_sql  = 'ORDER BY ss_order_index ASC,';
+
+    $sql = str_replace('SELECT', $select_sql, $sql);
+    $sql = str_replace('ORDER BY', $order_sql, $sql);
+    $sql = str_replace('WHERE', $where_sql, $sql);
+
+    $this->sql = $sql;
+  }
 
     
 }
--- third_party/supersticky/tests/test.mod_supersticky.php
+++ third_party/supersticky/tests/test.mod_supersticky.php
@@ -8,7 +8,7 @@
  * @package         Supersticky
  */
 
-require_once PATH_THIRD .'supersticky/mcp.supersticky.php';
+require_once PATH_THIRD .'supersticky/mod.supersticky.php';
 require_once PATH_THIRD .'supersticky/tests/mocks/mock.supersticky_model.php';
 
 class Test_supersticky extends Testee_unit_test_case {
@@ -38,6 +38,96 @@ class Test_supersticky extends Testee_unit_test_case {
 
         $this->_model   = $this->_ee->supersticky_model;
         $this->_subject = new Supersticky();
+    }
+
+
+    public function test__build_sql_query__works_with_basic_data()
+    {
+      $group_id     = 10;
+      $querystring  = '';
+
+      $this->_ee->session->expectOnce('userdata', array('group_id'));
+      $this->_ee->session->setReturnValue('userdata', $group_id);
+
+      $ss_entries = array(
+        new Supersticky_entry(array(
+          'entry_id' => '300',
+          'criteria' => array(
+            new Supersticky_criterion(array(
+              'date_from' => new DateTime('-1 day'),
+              'date_to'   => new DateTime('+ 1 day'),
+              'member_groups' => array(10, 20, 30)
+            ))
+          )
+        )),
+        new Supersticky_entry(array(
+          'entry_id' => '100',
+          'criteria' => array(
+            new Supersticky_criterion(array(
+              'date_from' => new DateTime('-1 day'),
+              'date_to'   => new DateTime('+ 1 day'),
+              'member_groups' => array(10)
+            ))
+          )
+        ))
+      );
+
+      $this->_model->expectOnce('get_supersticky_entries_for_date',
+        array(new DateTime()));
+
+      $this->_model->setReturnValue('get_supersticky_entries_for_date',
+        $ss_entries);
+
+      // Set the current SQL.
+      $initial_sql = "SELECT a.b FROM c WHERE d = 'e' ORDER BY f ASC";
+
+      $order_sql = 'ORDER BY ss_order_index ASC,';
+
+      $where_sql = 'LEFT JOIN (
+          SELECT 300 AS entry_id, 1 AS order_index
+          UNION ALL
+          SELECT 100, 2
+        ) AS ss ON ss.entry_id = t.entry_id
+        WHERE';
+
+      $expected_result = str_replace(
+        'SELECT',
+        'SELECT IFNULL(ss.order_index, 999999) AS ss_order_index,',
+        $initial_sql
+      );
+
+      $expected_result  = str_replace('ORDER BY', $order_sql, $expected_result);
+      $expected_result  = str_replace('WHERE', $where_sql, $expected_result);
+      
+      $this->_subject->sql = $initial_sql;
+      $this->_subject->build_sql_query($querystring);
+
+      $actual_result = $this->_subject->sql;
+
+      /**
+       * HACK ALERT:
+       * We need to ignore whitespace when checking that the actual result
+       * and the expected result are equal.
+       */
+
+      $pattern = '/[\t\n ]+/';
+
+      $this->assertIdentical(
+        preg_replace($pattern, '', $expected_result),
+        preg_replace($pattern, '', $actual_result)
+      );
+    }
+
+
+    public function test__build_sql_query__works_with_no_supersticky_entries()
+    {
+
+    }
+
+
+    public function test__build_sql_query__ignores_non_member_group_entries()
+    {
+
     }
 
 
